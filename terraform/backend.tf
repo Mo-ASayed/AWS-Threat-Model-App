a0a26b3d3783d0299e4fb7655b8a5f18feb81b6c
Mohammed Sayed <mohammedsayed9607@gmail.com>
2024-12-12 08:23:24 +0000

    Updated s3 with dynamoDB state lock

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,22 @@ terraform {
     key     = "state"
     region  = "us-east-1"
     encrypt = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST" 
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "Dev"
+    Purpose     = "Terraform State Locking"
   }
 }
