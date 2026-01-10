290483b59fc3f762eb875c0fc1b5874b283f2874
Mohammed Sayed <mohammedsayed9607@gmail.com>
2026-01-10 23:47:13 +0000

    create the router53 records

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -1,11 +1,20 @@
-data "aws_route53_zone" "tm_lab_zone" {
+resource "aws_route53_zone" "this" {
   name = var.zone_name
 }
 
-resource "aws_route53_record" "tm_cname_record" {
-  zone_id = data.aws_route53_zone.tm_lab_zone.id
+resource "aws_route53_record" "cname" {
+  zone_id = aws_route53_zone.this.id
   name    = var.record_name
   type    = "CNAME"
   ttl     = var.ttl
   records = [var.alb_dns_name]
 }
+
+resource "aws_route53_record" "tm_cname_record" {
+  zone_id = aws_route53_zone.tm_lab_zone.id
+  name    = var.record_name       # "tm.lab.mohammedsayed.com"
+  type    = "CNAME"
+  ttl     = var.ttl
+  records = [var.alb_dns_name]   # module.alb.alb_dns_name
+}
+
